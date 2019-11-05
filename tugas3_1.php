d51569502cfcf9673c56637c9b71f972886c24d1
wildan <wildan.syarif1021@gmail.com>
2019-11-05 17:14:13 +0700

    tugas niomic
    tugas niomic

---
+++ tugas3_1.php
@@ -0,0 +1,15 @@
+<?php
+
+    $jrk=360;
+    $kec=90;
+    $wktu=$jrk/$kec*60;
+
+    echo "Jarak kota A - Z = ".$jrk." Km";
+    print ("<br />");
+    echo "Kecepatan motor = ".$kec;
+    print ("<br />");    
+    echo "Waktu tempuh = ".$wktu;
+    print ("<br />");
+    echo "Jawaban : ".$wktu." Menit";
+
+?>
---
+++ tugas3_2.php
@@ -0,0 +1,15 @@
+<?php
+
+    $awal=150000;
+    $bunga=12.5;
+    $akhir=$awal*$bunga/100;
+
+    echo "Tabungan Awal = Rp ".$awal;
+    print ("<br />");
+    echo "Bunga Setahun = ".$bunga."%";
+    print ("<br />");    
+    echo "Total Tabungan Setahun = ".$akhir;
+    print ("<br />");
+    echo "Jawaban : Rp ".$akhir;
+
+?>
